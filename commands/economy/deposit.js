a30f595503cad42339800f43c51fc75fe43127b4
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-04-14 23:34:04 +0530

    Fix: Fixed deposit

--- commands/economy/deposit.js
+++ commands/economy/deposit.js
@@ -1,5 +1,5 @@
 const economy = require('../../scripts/economy');
-const response = require('./JSON/search.json'),
+const response = require('./JSON/work.json'),
 emotes = require('../../JSON/emotes.json');
 const { MessageEmbed } = require('discord.js');
 
@@ -8,6 +8,12 @@ module.exports = {
     execute: async (message,args) => {
         const walletbal = await economy.balance(message.author.id)
         if (!args[1]) return message.reply('What are you depositing nerd?')
+        if (args[1].toLowerCase() === 'all'){
+            await economy.deposit(message.author.id , walletbal)
+            await economy.addKR(message.author.id , -walletbal)
+            message.reply(`Withdrawn ${emotes.kr}${bankbal}`)
+            return;
+        }
         if (isNaN(args[1])) return message.reply('Sorry fam you can only deposit actual KR')
         if (args[1] > walletbal) return message.reply('What are you doing? you don\'t have that much kr')
         const depbal = await economy.deposit(message.author.id , args[1])
