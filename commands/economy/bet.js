041d354ad7e739ade6cdf8e4765a8aa3f089365b
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-15 01:17:42 +0530

    Add files via upload

--- commands/economy/bet.js
+++ commands/economy/bet.js
@@ -7,9 +7,32 @@ module.exports = {
     name: 'bet',
     cooldown: 10,
     execute: async (message , args) => {
-        if (!args[1]) return message.reply('What are you betting nerd?')
-        if (isNaN(args[1])) return message.reply('What do I look like to you? Provide a valid amount to bet')
         const wallet = await economy.balance(message.author.id)
+        if (!args[1]) return message.reply('What are you betting nerd?')
+        if (args[1].toLowerCase() === 'all'){
+            if (wallet <= 0) return message.reply(`You do not have any ${emotes.kr} in your wallet`)
+        const res = Math.floor(Math.random() * 2)
+        if (res === 1) {
+            await economy.addKR(message.author.id, wallet)
+            message.reply(new MessageEmbed()
+                .setColor('GREEN')
+                .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: false }))
+                .setDescription(`Lucky ducky , you won the bet! ${emotes.kr}${wallet}`)
+                .setFooter('stonks4u'))
+        } else {
+            await economy.addKR(message.author.id, wallet)
+            message.reply(new MessageEmbed()
+                .setColor('RED')
+                .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: false }))
+                .setDescription(`LMAO you lost the bet!`)
+                .setFooter('sucks to suck'))
+
+        }
+
+            return;
+        }
+        if (isNaN(args[1])) return message.reply('What do I look like to you? Provide a valid amount to bet')
+        
         if (wallet < args[1]) return message.reply(`You do not have ${args[1]} in your wallet`)
         if (args[1] <= 0) return message.reply('How about you try to provide an actual number?')
         const res = Math.floor(Math.random() * 2)
--- commands/economy/deposit.js
+++ commands/economy/deposit.js
@@ -12,10 +12,10 @@ module.exports = {
         if (args[1].toLowerCase() === 'all'){
             await economy.deposit(message.author.id , walletbal)
             await economy.addKR(message.author.id , -walletbal)
-            message.reply(`Withdrawn ${emotes.kr}${bankbal}`)
+            message.reply(`Deposited ${emotes.kr}${walletbal}`)
             return;
         }
-        if (walletbal <= 0) return message.reply('Fam you cant deposit thin air')
+
         if (isNaN(args[1])) return message.reply('Sorry fam you can only deposit actual KR')
         if (args[1] <= 0) return message.reply(`What are you doing? Provide an actual number`)
         if (args[1] > walletbal) return message.reply('What are you doing? you don\'t have that much kr')
