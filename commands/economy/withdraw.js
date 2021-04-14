7636e33a3bb1190b02e693d8c33a217a6262a3fd
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-04-14 23:34:29 +0530

    Fix: Fixed withdraw

--- commands/economy/withdraw.js
+++ commands/economy/withdraw.js
@@ -1,5 +1,5 @@
 const economy = require('../../scripts/economy');
-const response = require('./JSON/search.json'),
+const response = require('./JSON/work.json'),
 emotes = require('../../JSON/emotes.json');
 const { MessageEmbed } = require('discord.js');
 
@@ -8,10 +8,18 @@ module.exports = {
     execute: async (message,args) => {
         const bankbal = await economy.bankBalance(message.author.id)
         if (!args[1]) return message.reply('What are you depositing nerd?')
-        if (isNaN(args[1])) return message.reply(`Sorry fam you can only deposit actual KR ${emotes.kr}`)
+        if (args[1].toLowerCase() === 'all'){
+            await economy.addKR(message.author.id , bankbal)
+            await economy.deposit(message.author.id , -bankbal)
+            message.reply(`Withdrawn ${emotes.kr}${bankbal}`)
+            return;
+        }
+        if (isNaN(args[1])) return message.reply(`Sorry fam you can only withdraw actual KR ${emotes.kr}`)
+        if (args[1] <= 0) return message.reply(`What are you doing? Provide an actual number`)
         if (args[1] > bankbal) return message.reply('What are you doing? you don\'t have that much kr')
         const walletbal = await economy.addKR(message.author.id , args[1])
         const addKR = await economy.deposit(message.author.id , -args[1])
         message.reply(`Withdrew ${emotes.kr}${args[1]} , current bank balance is ${emotes.kr}${addKR}`)
+
     }
 }
