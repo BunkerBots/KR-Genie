769666932758f4a1339b6cdd2d0818b9ad4658f2
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-14 20:27:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,72 @@
-const { Client , Collection , MessageEmbed } = require('discord.js'),
-core = require('./JSON/core.json'),
-mongoose = require('mongoose'),
-config = require('./JSON/config.json'),
-id = require('./JSON/id.json'),
-logger = require('./scripts/logger.js'),
-bot = new Client({disableMentions: 'everyone'}),
-fs = require('fs'),
-dev = require('./JSON/dev.json'),
-cooldowns = new Collection();
+const { Client, Collection, MessageEmbed } = require('discord.js'),
+    core = require('./JSON/core.json'),
+    mongoose = require('mongoose'),
+    config = require('./JSON/config.json'),
+    id = require('./JSON/id.json'),
+    logger = require('./scripts/logger.js'),
+    bot = new Client({ disableMentions: 'everyone' }),
+    fs = require('fs'),
+    dev = require('./JSON/dev.json'),
+    cooldowns = new Collection();
 require('dotenv').config()
 bot.commands = new Collection();
 const commandFolders = fs.readdirSync('./commands');
-for (const folder of commandFolders){
+for (const folder of commandFolders) {
     const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles){
+    for (const file of commandFiles) {
         const command = require(`./commands/${folder}/${file}`)
         bot.commands.set(command.name, command);
     }
 }
 
 bot.on('ready', async () => {
-    logger.info(`Logged in as ${bot.user.username}`)
+    module.exports.bot = bot;
+    logger.debug('index.js', 'Logging in');
+    logger.info('Ready!');
     bot.user.setPresence({
         activity: {
-            name: 'KR fly by',
+            name: `KR fly by`,
             type: 'WATCHING'
         },
         status: 'idle'
     })
-    mongoose.connect(config.mongoPath , {
+    await logger.init(bot);
+    process.on('unhandledRejection', logger.unhandledError);
+    
+    mongoose.connect(config.mongoPath, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
+    }).then(() => {
+        logger.info('Connected to mongoDB!')
     })
-    console.log(config.mongoPath)
+    console.log('bot is on ggs')
+    //console.log(config.mongoPath)
+    bot.channels.resolve(id.channels.logs).send(new MessageEmbed()
+        .setDescription(`\`\`\`diff\n+ Logged in as ${bot.user.username}\n- Version : ${core.version}\`\`\`\nDatabase: MongoDB\nstatus: connected <a:check:827647433445474314>`)
+        .setTimestamp()).catch(console.error)
+    process.on('SIGTERM', () => {
+        bot.user.setPresence({
+            activity: {
+                name: 'SHUTTING DOWN',
+                type: 'WATCHING',
+            },
+            status: 'dnd',
+        }).then(() => {
+            logger.info('SHUT DOWN!');
+            process.exit(0);
+        }).catch(() => {
+            logger.info('SHUTTING DOWN WITHOUT PRESENCE!');
+            process.exit(0);
+        });
+    });
 })
 
-bot.on('message' , async message => {
+bot.on('message', async message => {
     if (message.author.bot) return;
     if (!message.content.startsWith(core.prefix)) return;
     const args = message.content.substring(core.prefix.length).trim().split(" "),
-    commandName = args[0].toLowerCase();
+        commandName = args[0].toLowerCase();
     const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandname));
     if (!command) return;
     if (!cooldowns.has(command.name)) {
@@ -58,10 +84,10 @@ bot.on('message' , async message => {
             if (now < expirationTime) {
                 const timeLeft = (expirationTime - now) / 1000;
                 return message.reply(new MessageEmbed()
-                .setColor('YELLOW')
-                .setTitle('Whoa whoa hold on...')
-                .setDescription(`You need to wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
-                .setFooter('notstonks4u'));
+                    .setColor('YELLOW')
+                    .setTitle('Whoa whoa hold on...')
+                    .setDescription(`You need to wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
+                    .setFooter('notstonks4u'));
             }
         }
     }
@@ -69,7 +95,7 @@ bot.on('message' , async message => {
     timestamps.set(message.author.id, now);
     setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
     try {
-        command.execute( message , args)
+        command.execute(message, args)
     } catch (error) {
         console.log(error)
     }
