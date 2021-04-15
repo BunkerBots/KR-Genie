c85fb051869ac4b7792894ebfb78d9f0987e37c1
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-15 17:12:40 +0530

    neater management

---
+++ data/dependencies.js
@@ -0,0 +1,8 @@
+const emotes = require('../JSON/emotes.json'),
+    profileSchema = require('../schemas/profile-schema'),
+    economy = require('../scripts/economy'),
+    crime = require('../commands/economy/JSON/crime.json'),
+    work = require('../commands/economy/JSON/work.json'),
+    beg = require('../commands/economy/JSON/beg.json')
+
+module.exports = {emotes , profileSchema , economy , crime , work , beg}
