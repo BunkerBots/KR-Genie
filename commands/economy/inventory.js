0af5ac08805a7ffcf04566536a4858c8e4933c2b
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-16 23:54:08 +0530

    Update inventory.js

--- commands/economy/inventory.js
+++ commands/economy/inventory.js
@@ -6,7 +6,6 @@ module.exports = {
         //if (!dependencies.developers.developers.includes(message.author.id)) return;
         if (!args[1]) {
             const data = await dependencies.economy.skinInventory(message.author.id)
-            console.log(data.length)
             let res = ''
             for (const skins of data) {
                 res += `\n[${skins.name}](${skins.link}) x ${skins.count}`
@@ -14,7 +13,7 @@ module.exports = {
             console.log(data)
             const embed = new MessageEmbed()
                 .setAuthor(`${message.author.username}'s Inventory`, message.author.displayAvatarURL({ dynamic: false }))
-                .setDescription(res)
+                .setDescription('Skin listing cooming soon™')
                 .setTitle(`Skin count - ${data.length}`)
             message.channel.send(embed)
             return;
@@ -26,8 +25,7 @@ module.exports = {
             let res = ''
             for (const skins of data) {
                 res += `\n[${skins.name}](${skins.link}) x ${skins.count}`
-            }
-            console.log(data)
+            
             const embed = new MessageEmbed()
                 .setAuthor(`${user.username}'s Inventory`, user.displayAvatarURL({ dynamic: false }))
                 .setDescription('Skin listing cooming soon™')
