daeb9853979a17baa9deaecde8b4554d5dae37b9
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-04-16 22:06:01 +0530

    Chore: Added id to data
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- data/index.js
+++ data/index.js
@@ -5,7 +5,8 @@ const emotes = require('./JSON/emotes.json'),
     beg = require('./JSON/beg.json'),
     core = require('./JSON/core.json'),
     betaTesters = require('./JSON/betaTesters.json'),
+    id = require('./JSON/id.json'),
     profileSchema = require('../schemas/profile-schema'),
     economy = require('../scripts/economy');
 
-module.exports = { emotes, profileSchema, economy, crime, work, beg, developers, core, betaTesters };
+module.exports = { emotes, profileSchema, economy, crime, work, beg, developers, core, betaTesters, id };
