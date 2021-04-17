d19a9f5e1f547bbe4ebd32ade14448c99d1c082c
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-18 03:18:50 +0530

    Create test.js

---
+++ commands/test/test.js
@@ -0,0 +1,16 @@
+const { MessageEmbed, Message } = require('discord.js')
+const dependencies = require('../../data')
+const skinfetcher = require('../../data/skins')
+module.exports = {
+    name: 'test',
+    aliases: ['search'],
+    execute: async (m, a) => {
+        let arr = [];
+        m.guild.members.cache.forEach(async member => {
+            const wallet = await dependencies.economy.balance(member.id)
+            arr.push(wallet)
+            console.log(wallet)
+        })
+        console.log(arr)
+    }
+}
