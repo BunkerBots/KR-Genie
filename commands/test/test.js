42d4953ddd37328797db40622a70dbb4cfec4cbc
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-18 03:28:01 +0530

    Update test.js

--- commands/test/test.js
+++ commands/test/test.js
@@ -3,14 +3,15 @@ const dependencies = require('../../data')
 const skinfetcher = require('../../data/skins')
 module.exports = {
     name: 'test',
-    aliases: ['search'],
+    aliases: ['lb'],
     execute: async (m, a) => {
         let arr = [];
         m.guild.members.cache.forEach(async member => {
             const wallet = await dependencies.economy.balance(member.id)
             arr.push(wallet)
             console.log(wallet)
-        })
+        }).then(() => {
         console.log(arr)
+        })
     }
 }
