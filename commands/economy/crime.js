c296bf1cd69cf372e22da9d6300538a8fd640ebb
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-20 08:12:24 +0530

    fixed chances

--- commands/economy/crime.js
+++ commands/economy/crime.js
@@ -4,10 +4,10 @@ module.exports = {
     name: 'crime',
     cooldown: 1000,
     execute: async(message) => {
+        const { wallet } = await data.economy.balance(message.author.id);
         const res = Math.floor(Math.random() * 100);
         if (res <= 10) {
             const deathresponse = data.crime.responses['death-response'][Math.floor(Math.random() * data.crime.responses['death-response'].length)];
-            const { wallet } = await data.economy.balance(message.author.id);
             await data.economy.addKR(message.author.id, -wallet);
             message.reply(new MessageEmbed()
                 .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: false }))
@@ -25,7 +25,7 @@ module.exports = {
                 .setFooter('stonks4u'));
         } else if (res > 50 && res <= 100) {
             const favourableresponse = data.crime.responses['non-favourable-response'][Math.floor(Math.random() * data.crime.responses['non-favourable-response'].length)];
-            const randomKR = Math.floor(Math.random() * 10000);
+            const randomKR = Math.floor(Math.random() * wallet);
             await data.economy.addKR(message.author.id, -randomKR);
             message.reply(new MessageEmbed()
                 .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: false }))
