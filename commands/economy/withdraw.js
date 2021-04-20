8e7fa17873ad38b705205dc6e917b469b993e427
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-20 17:56:56 +0530

    parseInt(args) bug fix #1

--- commands/economy/withdraw.js
+++ commands/economy/withdraw.js
@@ -4,18 +4,19 @@ module.exports = {
     aliases: ['withdraw'],
     execute: async(message, args) => {
         const { wallet , bank } = await data.economy.balance(message.author.id)
-        if (bank <= 0) return message.reply('What are you withdrawing? lmao');
+        if (bank <= 0) return message.reply(`You do not have any ${data.emotes.kr} in your bank!`);
         if (!args[1]) return message.reply('What are you withdrawing nerd?');
         if (args[1].toLowerCase() === 'all') {
-            await data.economy.withdraw(message.author.id , bank)
-            message.reply(`Withdrawn ${data.emotes.kr}${bankbal}`);
+            await data.economy.withdraw(message.author.id , parseInt(bank))
+            message.reply(`Withdrawn ${data.emotes.kr}${parseInt(bank)}`);
             return;
         }
+        let KRtowithdraw = parseInt(args[1]);
         if (bank < 0) return message.reply(`You don\'t have any ${data.emotes.kr} in your bank. lmfao`);
-        if (isNaN(args[1])) return message.reply(`Sorry fam you can only withdraw actual KR ${data.emotes.kr}`);
-        if (args[1] <= 0) return message.reply('What are you doing? Provide an actual number');
-        if (args[1] > bank) return message.reply('What are you doing? you don\'t have that much kr');
+        if (!Number.isInteger(args[1])) return message.reply(`Sorry fam you can only withdraw whole numbers ${data.emotes.kr}`);
+        if (KRtowithdraw <= 0) return message.reply(`You do not have any ${data.emotes.kr} in your bank!`);
+        if (KRtowithdraw > bank) return message.reply(`What are you doing? you don\'t have ${data.emotes.kr}${KRtowithdraw} in your bank`);
         await data.economy.withdraw(message.author.id , args[1])
-        message.reply(`Withdrew ${data.emotes.kr}${args[1]} , current bank balance is ${data.emotes.kr}${bank}`);
+        message.reply(`Withdrew ${data.emotes.kr}${KRtowithdraw} , current bank balance is ${data.emotes.kr}${parseInt(await data.economy.balance(message.author.id).bank)}`);
     },
 };
