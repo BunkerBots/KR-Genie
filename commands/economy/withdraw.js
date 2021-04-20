bbd91880f7c57b1c8c1f8f9e6cf3936bfa888564
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-20 15:28:40 +0530

    feat : revamp

--- commands/economy/withdraw.js
+++ commands/economy/withdraw.js
@@ -3,21 +3,19 @@ module.exports = {
     name: 'with',
     aliases: ['withdraw'],
     execute: async(message, args) => {
-        const bankbal = await data.economy.bankBalance(message.author.id);
-        if (bankbal <= 0) return message.reply('What are you withdrawing? lmao');
+        const { wallet , bank } = await data.economy.balance(message.author.id)
+        if (bank <= 0) return message.reply('What are you withdrawing? lmao');
         if (!args[1]) return message.reply('What are you withdrawing nerd?');
         if (args[1].toLowerCase() === 'all') {
-            await data.economy.addKR(message.author.id, bankbal);
-            await data.economy.deposit(message.author.id, -bankbal);
+            await data.economy.withdraw(message.author.id , bank)
             message.reply(`Withdrawn ${data.emotes.kr}${bankbal}`);
             return;
         }
-        if (bankbal < 0) return message.reply('What are you withdrawing? lmao');
+        if (bank < 0) return message.reply(`You don\'t have any ${data.emotes.kr} in your bank. lmfao`);
         if (isNaN(args[1])) return message.reply(`Sorry fam you can only withdraw actual KR ${data.emotes.kr}`);
         if (args[1] <= 0) return message.reply('What are you doing? Provide an actual number');
-        if (args[1] > bankbal) return message.reply('What are you doing? you don\'t have that much kr');
-        await data.economy.addKR(message.author.id, args[1]);
-        const addKR = await data.economy.deposit(message.author.id, -args[1]);
-        message.reply(`Withdrew ${data.emotes.kr}${args[1]} , current bank balance is ${data.emotes.kr}${addKR}`);
+        if (args[1] > bank) return message.reply('What are you doing? you don\'t have that much kr');
+        await data.economy.withdraw(message.author.id , args[1])
+        message.reply(`Withdrew ${data.emotes.kr}${args[1]} , current bank balance is ${data.emotes.kr}${bank}`);
     },
 };
