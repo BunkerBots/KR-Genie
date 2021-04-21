bb8050b873b64f8f0980372138c6753405c0727c
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-04-21 17:04:00 +0530

    Feat: Added iterators
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- scripts/economy.js
+++ scripts/economy.js
@@ -80,9 +80,78 @@ class DBClient {
         return this.get(id).then(x => x.inventory.skins);
     }
 
-}
-const client = new DBClient;
+    async* iterator() {
+        const streamer = await this.keyv.options.store.redis.scanStream({
+            match: `${this.keyv.options.namespace}:*`,
+        });
+        const iterator = streamify(streamer, 'data');
 
+        for await (const keys of iterator) {
+            for (const key of await keys)
+                yield key.split(':').splice(1);
+        }
+    }
+
+    async values() {
+        const iterator = this.iterator();
+        const keyPromise = [];
+        for await (const key of iterator)
+            keyPromise.push(await this.keyv.get(key));
+        return await Promise.all(keyPromise);
+    }
+
+}
+
+const streamify = function(stream, event) {
+    const pullQueue = [];
+    const pushQueue = [];
+    let done = false;
+    const pushValue = async(args) => {
+        if (pullQueue.length !== 0) {
+            const resolver = pullQueue.shift();
+            resolver(...args);
+        } else
+            pushQueue.push(args);
+    };
+
+    const pullValue = () => {
+        return new Promise((resolve) => {
+            if (pushQueue.length !== 0) {
+                const args = pushQueue.shift();
+                resolve(...args);
+            } else
+                pullQueue.push(resolve);
+        });
+    };
+
+    const handler = (...args) => {
+        pushValue(args);
+    };
+
+    stream.on(event, handler);
+    return {
+        [Symbol.asyncIterator]() {
+            return this;
+        },
+        next: () => ({
+            done,
+            value: done ? undefined : pullValue(),
+        }),
+        return: () => {
+            done = true;
+            stream.removeEventListener(event, handler);
+            return { done };
+        },
+        throw: (error) => {
+            done = true;
+            return {
+                done,
+                value: Promise.reject(error),
+            };
+        },
+    };
+};
+const client = new DBClient;
 const bench = {};
 if (process.env.BENCHMARK) {
     console.log('ENABLING BENCHMARKS!');
