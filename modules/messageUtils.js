1d9942158dc504bf352fd1fef34ab61271f81c4d
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-04-21 16:18:55 +0530

    Fixed msg utils

--- modules/messageUtils.js
+++ modules/messageUtils.js
@@ -1,8 +1,13 @@
 const { Message } = require('discord.js');
 
+Message.prototype.bind(getMentions)
 const mentionRegex = /<?@?!?(\d{17,19})>?/g
 /**
  * @param {Optional String} String to check for mentions in
  * @returns {Array(User ID)} Returns an array of User IDs, which are mentioned or are in the content
  */
-Message.prototype.getMentions = (args) =>  args ? args.match(mentionRegex) : this.content.match(mentionRegex);
+const getMentions = (args) =>  {
+    console.log(this);
+    return args ? args.match(mentionRegex) : this.content.match(mentionRegex);
+}
+Message.prototype.getMentions = getMentions;
