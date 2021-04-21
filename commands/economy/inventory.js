ef1496f300447544b2a9dac9f07c17ac7425e808
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-21 08:42:33 +0530

    initial commit

--- commands/economy/inventory.js
+++ commands/economy/inventory.js
@@ -6,7 +6,9 @@ module.exports = {
     execute: async (message, args) => {
         let skinsarr = []
         //if (!dependencies.developers.developers.includes(message.author.id)) return;
-        if (!args[1]) {
+        if (!args[1] || (Number.isInteger(parseInt(args[1])) && args[1].length < 5)) {
+            let footer;
+            let pageNumber;
             const data = await dependencies.economy.skinInventory(message.author.id)
             let res = ''
             for (const skins of data) {
@@ -21,53 +23,42 @@ module.exports = {
             if (!skinsarr.length) i = 'No data found'
             else i = skinsarr.join('\n')
             const guilds = skinsarr
+
             /**
-* Creates an embed with guilds starting from an index.
-* @param {number} start The index to start from.
-*/
+             * Creates an embed with guilds starting from an index.
+             * @param {number} start The index to start from.
+             */
             const generateEmbed = start => {
                 const current = guilds.slice(start, start + 10)
-
-                // you can of course customise this embed however you want
                 const embed = new MessageEmbed()
                     .setAuthor(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: false }))
-                    .setTitle('Your Inventory')
+                    .setTitle(`${message.author.username}'s Inventory`)
                     .setDescription(`Showing skins ${start + 1}-${start + current.length} out of ${guilds.length}`)
+                    .setFooter(footer)
                 current.forEach(g => embed.addField(g, '\u200b'))
                 return embed
             }
+            if (guilds.length < 10) {
+                footer = `1 out of 1`
+                message.channel.send(generateEmbed(0))
+                return;
+            }
 
-            // edit: you can store the message author like this:
-            const author = message.author
-
-            // send the embed with the first 10 guilds
-            message.channel.send(generateEmbed(0)).then(message => {
-                // exit if there is only one page of guilds (no need for all of this)
-                if (guilds.length <= 10) return
-                // react with the right arrow (so that the user can click it) (left arrow isn't needed because it is the start)
-                message.react('➡️')
-                const collector = message.createReactionCollector(
-                    // only collect left and right arrow reactions from the message author
-                    (reaction, user) => ['⬅️', '➡️'].includes(reaction.emoji.name) && user.id === author.id,
-                    // time out after a minute
-                    { time: 60000 }
-                )
-
-                let currentIndex = 0
-                collector.on('collect', reaction => {
-                    // remove the existing reactions
-                    message.reactions.removeAll().then(async () => {
-                        // increase/decrease index
-                        reaction.emoji.name === '⬅️' ? currentIndex -= 10 : currentIndex += 10
-                        // edit message with new embed
-                        message.edit(generateEmbed(currentIndex))
-                        // react with left arrow if it isn't the start (await is used so that the right arrow always goes after the left)
-                        if (currentIndex !== 0) await message.react('⬅️')
-                        // react with right arrow if it isn't the end
-                        if (currentIndex + 10 < guilds.length) message.react('➡️')
-                    })
-                })
-            })
+            if (!args[1]) {
+                let lastPage = guilds.length / 10
+                if (!Number.isInteger(lastPage)) lastPage = parseInt(lastPage.toFixed(0)) + 1
+                footer = `1 out of ${lastPage}`
+                message.channel.send(generateEmbed(0))
+            } else {
+                let lastPage = guilds.length / 10
+                if (!Number.isInteger(lastPage)) lastPage = parseInt(lastPage.toFixed(0)) + 1
+                footer = `${args[1]} out of ${lastPage}`
+                pageNumber = args[1] - 1
+                let currentindex = parseInt(pageNumber * 10)
+                console.log(currentindex)
+                if (currentindex > guilds.length) return;
+                message.channel.send(generateEmbed(currentindex))
+            }
 
             return;
         }
@@ -114,7 +105,7 @@ module.exports = {
                 return;
             }
 
-            if (!args[2]){
+            if (!args[2]) {
                 let lastPage = guilds.length / 10
                 if (!Number.isInteger(lastPage)) lastPage = parseInt(lastPage.toFixed(0)) + 1
                 footer = `1 out of ${lastPage}`
@@ -129,5 +120,6 @@ module.exports = {
                 message.channel.send(generateEmbed(currentindex))
             }
         })
+
     }
 }
