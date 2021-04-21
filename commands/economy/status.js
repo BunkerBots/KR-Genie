9c36aed371078a5030a100975e6ae29ed5d42146
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-21 06:43:54 +0530

    client fetch

--- commands/economy/status.js
+++ commands/economy/status.js
@@ -10,16 +10,40 @@ module.exports = {
             for (let i = 0; i < 7; i++)
                 sortedRarities[i] = Inventory.filter(x => x.rarity == i);
             message.channel.send(new MessageEmbed()
-            .setAuthor(message.author.username , message.author.displayAvatarURL({dynamic: false}))
-            .setTitle(`${message.author.username}'s Economy Stats`)
-            .setDescription(`Total spins : ${Inventory.length}`)
-            .addField(`Unobtainables:`, `${sortedRarities[6].length || 0}`)
-            .addField(`Contrabands:` , `${sortedRarities[5].length || 0}`)
-            .addField(`Relics:`, `${sortedRarities[4].length || 0}`)
-            .addField(`Legendaries` , `${sortedRarities[3].length || 0}`)
-            .addField(`Epics:` , `${sortedRarities[2].length || 0}`)
-            .addField(`Rares:` , `${sortedRarities[1].length || 0}`)
+                .setAuthor(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: false }))
+                .setTitle(`${message.author.username}'s Economy Stats`)
+                .setDescription(`Total spins : ${Inventory.length}`)
+                .addField(`Unobtainables:`, `${sortedRarities[6].length || 0}`)
+                .addField(`Contrabands:`, `${sortedRarities[5].length || 0}`)
+                .addField(`Relics:`, `${sortedRarities[4].length || 0}`)
+                .addField(`Legendaries`, `${sortedRarities[3].length || 0}`)
+                .addField(`Epics:`, `${sortedRarities[2].length || 0}`)
+                .addField(`Rares:`, `${sortedRarities[1].length || 0}`)
             )
+            return;
         }
+
+        const target = message.client.users.fetch(args[1].replace(/\D/g, ''));
+        try {
+            await target;
+        } catch (e) {
+            message.channel.send('Unknown user');
+        }
+        target.then(async user => {
+            const userInventory = await data.economy.skinInventory(user.id)
+            for (let i = 0; i < 7; i++)
+                sortedRarities[i] = userInventory.filter(x => x.rarity == i);
+            message.channel.send(new MessageEmbed()
+                .setAuthor(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: false }))
+                .setTitle(`${user.username}'s Economy Stats`)
+                .setDescription(`Total spins : ${Inventory.length}`)
+                .addField(`Unobtainables:`, `${sortedRarities[6].length || 0}`)
+                .addField(`Contrabands:`, `${sortedRarities[5].length || 0}`)
+                .addField(`Relics:`, `${sortedRarities[4].length || 0}`)
+                .addField(`Legendaries`, `${sortedRarities[3].length || 0}`)
+                .addField(`Epics:`, `${sortedRarities[2].length || 0}`)
+                .addField(`Rares:`, `${sortedRarities[1].length || 0}`)
+            )
+        })
     }
 }
