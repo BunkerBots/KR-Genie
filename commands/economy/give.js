d292d3eba5ac6bf6e3a3d71a10ac5ad1fd1e4d99
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-04-22 19:26:07 +0530

    Fix: Fixed argument parsing errors in status
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- commands/economy/give.js
+++ commands/economy/give.js
@@ -11,7 +11,7 @@ module.exports = {
         const krtogive = parseInt(args[1]);
         if (wallet <= 0) return message.channel.send(`You do not have enough ${data.emotes.kr}`);
         if (wallet < krtogive) return message.channel.send(`You do not have ${data.emotes.kr}${args[2]}`);
-        if (args[2].toLowerCase() === 'all') {
+        if (args[1].toLowerCase() === 'all') {
             await db.utils.addKR(user.id, parseInt(wallet));
             await db.utils.addKR(message.author.id, -parseInt(wallet));
             const authorallbal = await db.utils.balance(message.author.id);
@@ -20,7 +20,6 @@ module.exports = {
             return;
         }
         if (isNaN(args[1])) return message.channel.send('What are you doing? That\'s not even a valid number');
-        // const krtogive = parseInt(args[2])
         await db.utils.addKR(user.id, krtogive);
         await db.utils.addKR(message.author.id, -krtogive);
         const authorbal = await db.utils.balance(message.author.id);
