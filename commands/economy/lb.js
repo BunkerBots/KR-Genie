eba9b73a49ede53b6d6307b42fdb0a86af79ac9d
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-04-22 15:54:44 +0530

    Refactor: Refactored lb
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- commands/economy/lb.js
+++ commands/economy/lb.js
@@ -5,50 +5,26 @@ const db = require('../../modules/');
 
 module.exports = {
     name: 'lb',
-    execute: async(message) => {
-        const arr = [new Object()];
-        const embedArr = [];
-        let sorter;
-        if (message.content.includes('--cash')) sorter = (x, y) => x.balance.wallet - y.balance.wallet;
-        else sorter = (x, y) => x.balance.wallet + x.balance.bank - (y.balance.wallet + y.balance.bank);
+    execute: async(message, args) => {
+        const sortByCash = message.content.includes('--cash');
+        message.content = message.content.replace('--cash', '');
+        const sorter = sortByCash ? (x, y) => x.balance.wallet - y.balance.wallet : (x, y) => x.balance.wallet + x.balance.bank - (y.balance.wallet + y.balance.bank);
         const values = (await db.values()).sort(sorter).reverse();
-        for (const i of values) {
-            const bankBal = i.balance.bank;
+        const max = Math.floor(values.length / 10);
+        let page = args[1] || 1;
+        if (page <= 0) return message.reply('Page no. has to be greater than 0, nitwit');
+        if (page > max) page = max;
+        const lbUsers = [];
+        for (const i of values.splice(page, 10)) {
+            const bankBal = i.balance.wallet + sortByCash ? 0 : i.balance.bank;
             const user = await utils.getID(i.id);
-            arr.push({ name: user.username, bal: bankBal });
+            lbUsers.push({ name: user.username, balance: bankBal });
         }
-        const splicedarr = arr.splice(1);
-        console.log(splicedarr)
-        for (let i = 0; i < splicedarr.length ; i++) {
-            embedArr.push(`${parseInt([i]) + 1} ${splicedarr[i].name} : ${data.emotes.kr}${splicedarr[i].bal.bank}`);
-            if (values.length < 11) {
-                // eslint-disable-next-line no-undef
-                for (i = 0; i < values.length ; i++) {
-                    // console.log(i)
-                    // eslint-disable-next-line no-undef
-                    embedArr.push(`\`${parseInt([i]) + 1}.\` ${values[i].name} : ${data.emotes.kr}${values[i].bal.wallet}`);
-                }
-                const embed = new MessageEmbed()
-                    .setAuthor(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: false }))
-                    .setTitle('Top 10 Richest Users')
-                    .setDescription(`${embedArr.join('\n\u200b\n')}`)
-                    .setFooter('These are wallet balance');
-                console.log(embedArr);
-                message.channel.send(embed);
-            } else {
-            // eslint-disable-next-line no-undef
-                for (i = 0; i < 10 ; i++) {
-                    console.log(values[i]);
-                    embedArr.push(`\`${parseInt([i]) + 1}.\` ${values[i].name} : ${data.emotes.kr}${values[i].balance.wallet}`);
-                }
-                const embed = new MessageEmbed()
-                    .setAuthor(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: false }))
-                    .setTitle('Top 10 Richest Users')
-                    .setDescription(`${embedArr.join('\n\u200b\n')}`)
-                    .setFooter('These are wallet balance');
-                console.log(embedArr);
-                message.channel.send(embed);
-            }
-        }
+        const embed = new MessageEmbed()
+            .setTitle('Global Leaderboard ' + sortByCash ? 'Cash' : 'Networth')
+            .setDescription(toString(lbUsers))
+            .setFooter(`Page ${page}/${max}`);
+        message.channel.send(embed);
     },
 };
+const toString = (users) => users.map((x, i) => `**${Number(i) + 1}.** ${x.name} - ${x.balance}`).join('\n');
