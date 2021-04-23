bd1eec376bba27eb483b5e62f59fd1d745b5bf02
EJ BEAN <danonymousarmy@gmail.com>
2021-04-24 00:18:57 +0530

    dev update

--- commands/economy/profile.js
+++ commands/economy/profile.js
@@ -4,18 +4,21 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'userinfo',
     aliases: ['profile'],
+    cooldown: 10,
     execute: async(message, args) => {
         if (!args[0]) {
             let krunkitisEmote;
             let premiumEmote;
             let verifiedEmote;
+            let devEmote;
             const krunkitis = await db.utils.krunkitis(message.author.id);
             const premium = await db.utils.premium(message.author.id);
             const verified = await db.utils.verified(message.author.id);
             const { wallet, bank } = await db.utils.balance(message.author.id);
             const netWorth = parseInt(wallet + bank);
             const inventory = await db.utils.skinInventory(message.author.id);
-            console.log(`${krunkitis} ${premium}`);
+            if (data.devs.includes(message.author.id)) devEmote = ':tools:';
+            else devEmote = '';
             if (krunkitis == true) krunkitisEmote = data.emotes.krunkitis;
             else krunkitisEmote = '';
             if (premium == true) premiumEmote = data.emotes.premium;
@@ -24,7 +27,7 @@ module.exports = {
             else verifiedEmote = '';
             const embed = new MessageEmbed()
                 .setAuthor(`${message.author.username}`)
-                .setTitle(`${krunkitisEmote} ${premiumEmote} ${verifiedEmote}`)
+                .setTitle(`${krunkitisEmote} ${premiumEmote} ${verifiedEmote} ${devEmote}`)
                 .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
                 .setDescription('*biography coming soon™*')
                 .addFields(
@@ -40,17 +43,18 @@ module.exports = {
 
         const user = await message.client.users.fetch(args[0].replace(/\D/g, '')).catch(() => {});
         if (!user) return message.channel.send('Unknown user');
-        console.log(user);
         let krunkitisEmote;
         let premiumEmote;
         let verifiedEmote;
+        let devEmote;
         const krunkitis = await db.utils.krunkitis(user.id);
         const premium = await db.utils.premium(user.id);
         const verified = await db.utils.verified(user.id);
         const { wallet, bank } = await db.utils.balance(user.id);
         const netWorth = parseInt(wallet + bank);
         const inventory = await db.utils.skinInventory(user.id);
-        console.log(`${krunkitis} ${premium}`);
+        if (data.devs.includes(user.id)) devEmote = ':tools:';
+        else devEmote = '';
         if (krunkitis == true) krunkitisEmote = data.emotes.krunkitis;
         else krunkitisEmote = '';
         if (premium == true) premiumEmote = data.emotes.premium;
@@ -59,7 +63,7 @@ module.exports = {
         else verifiedEmote = '';
         const embed = new MessageEmbed()
             .setAuthor(`${user.username}`)
-            .setTitle(`${krunkitisEmote} ${premiumEmote} ${verifiedEmote}`)
+            .setTitle(`${krunkitisEmote} ${premiumEmote} ${verifiedEmote} ${devEmote}`)
             .setThumbnail(user.displayAvatarURL({ dynamic: true }))
             .setDescription('*biography coming soon™*')
             .addFields(
