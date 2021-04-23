468102c12cb2c90493b4bb690bc096df22600d79
EJ BEAN <danonymousarmy@gmail.com>
2021-04-23 23:17:59 +0530

    Feat : dev command - unverify

---
+++ commands/dev/unverify.js
@@ -0,0 +1,12 @@
+const db = require('../../modules');
+const id = require('../../data');
+module.exports = {
+    name: 'unverify',
+    execute: async(message, args) => {
+        if (!id.devs.includes(message.author.id)) return;
+        const target = await message.client.users.fetch(args[0].replace(/\D/g, '')).catch(() => {});
+        if (!target) return message.channel.send('Unknown user');
+        db.util.unverify(target.id);
+        message.channel.send(`Successfully unverified \`${target.username}\``);
+    },
+};
