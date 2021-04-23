e0f25a0884a6feb4f9718a868ed935c615e1412d
EJ BEAN <danonymousarmy@gmail.com>
2021-04-23 23:21:39 +0530

    Feat : dev commands [cure , infect]

---
+++ commands/dev/cure.js
@@ -0,0 +1,12 @@
+const db = require('../../modules');
+const data = require('../../data');
+module.exports = {
+    name: 'cure',
+    execute: async(message, args) => {
+        if (!data.devs.includes(message.author.id)) return;
+        const target = await message.client.users.fetch(args[0].replace(/\D/g, '')).catch(() => {});
+        if (!target) return message.channel.send('Unknown user');
+        db.util.cure(target.id);
+        message.channel.send(`Successfully cured \`${target.username}\` ${data.emotes.krunkitis}`);
+    },
+};
---
+++ commands/dev/infect.js
@@ -0,0 +1,12 @@
+const db = require('../../modules');
+const data = require('../../data');
+module.exports = {
+    name: 'infect',
+    execute: async(message, args) => {
+        if (!data.devs.includes(message.author.id)) return;
+        const target = await message.client.users.fetch(args[0].replace(/\D/g, '')).catch(() => {});
+        if (!target) return message.channel.send('Unknown user');
+        db.util.infect(target.id);
+        message.channel.send(`Successfully infected \`${target.username}\` ${data.emotes.krunkitis}`);
+    },
+};
