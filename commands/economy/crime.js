ed8e3c985227b43891786ae26bf373a0fd00be5a
EJ BEAN <danonymousarmy@gmail.com>
2021-04-24 15:00:38 +0530

    feat: embed color fix

--- commands/economy/crime.js
+++ commands/economy/crime.js
@@ -8,15 +8,17 @@ module.exports = {
     execute: async(message) => {
         const { wallet } = await db.utils.balance(message.author.id);
         const res = Math.floor(Math.random() * 100);
-        let description, footer, kr;
+        let description, footer, kr, color;
         if (res <= 10) {
             description = data.crime.responses['death-response'][Math.floor(Math.random() * data.crime.responses['death-response'].length)];
             footer = 'notstonks4u';
+            color = 'RED';
         } else if (res > 10 && res <= 50) {
             const favourableresponse = data.crime.responses['favourable-response'][Math.floor(Math.random() * data.crime.responses['favourable-response'].length)];
             const randomKR = parseInt(Math.floor(Math.random() * 10000));
             description = `${favourableresponse.replace('[kr]', `${data.emotes.kr}${randomKR}`)}`;
             footer = 'stonks4u';
+            color = 'GREEN';
             kr = randomKR;
         } else if (res > 50 && res <= 100) {
             const favourableresponse = data.crime.responses['non-favourable-response'][Math.floor(Math.random() * data.crime.responses['non-favourable-response'].length)];
@@ -25,14 +27,15 @@ module.exports = {
             if (wallet <= 0) randomKR = 0;
             else randomKR = resp;
             kr = -randomKR;
+            color = 'GREEN';
             description = `${favourableresponse.replace('[kr]', `${data.emotes.kr}${randomKR}`)}`;
-            footer = 'stonks4u';
+            footer = 'notstonks4u';
         }
         if (kr) await db.utils.addKR(message.author.id, kr);
         message.reply(
             new MessageEmbed()
                 .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: false }))
-                .setColor('RED')
+                .setColor(color)
                 .setDescription(description)
                 .setFooter(footer),
         );
