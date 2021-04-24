666705c228788b19cff5697865495d2a8f4531a2
EJ BEAN <danonymousarmy@gmail.com>
2021-04-24 16:14:17 +0530

    feat: perms fix

--- commands/dev/add-bal.js
+++ commands/dev/add-bal.js
@@ -3,10 +3,10 @@ const db = require('../../modules');
 module.exports = {
     name: 'add',
     execute: async(message, args) => {
-        if (!data.devs.includes(message.author.id) || !data.staff.includes(message.author.id)) return;
+        if (!(data.devs.includes(message.author.id) || data.staff.includes(message.author.id))) return;
         if (!args[0]) return message.reply(`Provide an user to add ${data.emotes.kr} to!`);
         const user = await message.client.users.fetch(args[0].replace(/\D/g, '')).catch(() => {});
-        if (!user) message.reply('Unkown user!');
+        if (!user) return message.reply('Unkown user!');
 
         const KR = args[1];
         if (isNaN(KR)) {
--- commands/dev/unverify.js
+++ commands/dev/unverify.js
@@ -3,7 +3,7 @@ const data = require('../../data');
 module.exports = {
     name: 'unverify',
     execute: async(message, args) => {
-        if (!data.devs.includes(message.author.id) || !data.staff.includes(message.author.id)) return;
+        if (!(data.devs.includes(message.author.id) || data.staff.includes(message.author.id))) return;
         const target = await message.client.users.fetch(args[0].replace(/\D/g, '')).catch(() => {});
         if (!target) return message.channel.send('Unknown user');
         await db.utils.unverify(target.id);
