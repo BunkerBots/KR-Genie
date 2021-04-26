4c2d4e1466bad4841f1058f84ee1fbc97faf03b5
EJ BEAN <danonymousarmy@gmail.com>
2021-04-26 17:22:35 +0530

    feat : level up rewards

--- mongo/index.js
+++ mongo/index.js
@@ -1,8 +1,10 @@
-const mongo = require('./mongo');
-const schema = require('./schema');
+const mongo = require('./mongo'),
+    schema = require('./schema'),
+    db = require('../modules'),
+    emotes = require('../data').emotes;
 
 const getNeededXP = (level) => level * level * 100;
-
+const levelReward = (level) => level * 1000;
 const addXP = async(userId, xpToAdd, message) => {
     await mongo().then(async() => {
         try {
@@ -24,16 +26,15 @@ const addXP = async(userId, xpToAdd, message) => {
 
             let { xp, level } = result;
             const needed = getNeededXP(level);
-
+            const reward = levelReward(level);
             if (xp >= needed) {
                 ++level;
                 xp -= needed;
 
                 message.reply(
-                    `You are now level ${level} with ${xp} experience! You now need ${getNeededXP(
-                        level,
-                    )} XP to level up again.`,
+                    `You leveled up! \`${level - 1} => ${level}\` with \`${xp}\` experience! As a reward ${emotes.kr}${parseInt(reward)} has been placed in your wallet!`,
                 );
+                await db.utils.addKR(userId, parseInt(reward));
 
                 await schema.updateOne(
                     {
