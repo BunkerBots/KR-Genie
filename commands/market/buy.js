3fa9c105ea7752447d48c5b1a205f1b019ef1e07
EJ BEAN <danonymousarmy@gmail.com>
2021-04-28 01:39:11 +0530

    fix : addkr function

--- commands/market/buy.js
+++ commands/market/buy.js
@@ -18,20 +18,20 @@ module.exports = {
         if (found) {
             if (found.type === 'c') {
                 await db.utils.addItem(message.author.id, found.id);
-                await db.utils.addKR(message.author.id, parseInt(found.price));
+                await db.utils.addKR(message.author.id, -parseInt(found.price));
                 message.channel.send(new MessageEmbed()
                     .setAuthor(`Successfully purchased ${found.name}`, message.author.displayAvatarURL({ dynamic: false }))
                     .setDescription(`<@${message.author.id}> bought **${found.icon} ${found.name}** and paid ${emotes.kr}${comma(found.price)}`)
                     .setFooter('Thank you for the purchase!'));
             } else if (found.type === 'b') {
-                await db.utils.addKR(message.author.id, parseInt(found.price));
+                await db.utils.addKR(message.author.id, -parseInt(found.price));
                 await db.utils.getPremium(message.author.id);
                 message.channel.send(new MessageEmbed()
                     .setAuthor(`Successfully purchased ${found.name}`, message.author.displayAvatarURL({ dynamic: false }))
                     .setDescription(`<@${message.author.id}> bought **${found.icon} ${found.name}** and paid ${emotes.kr}${comma(found.price)}`)
                     .setFooter('Thank you for the purchase!'));
             } else if (found.type === 's') {
-                await db.utils.addKR(message.author.id, parseInt(found.price));
+                await db.utils.addKR(message.author.id, -parseInt(found.price));
                 await db.utils.addSkin(message.author.id, found.index);
                 message.channel.send(new MessageEmbed()
                     .setAuthor(`Successfully purchased ${found.name}`, message.author.displayAvatarURL({ dynamic: false }))
