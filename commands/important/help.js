f11be706c117ddae4c1a52127bb656fece225417
EJ BEAN <danonymousarmy@gmail.com>
2021-04-29 21:44:22 +0530

    fix : typo

--- commands/important/help.js
+++ commands/important/help.js
@@ -8,9 +8,9 @@ module.exports = {
             const embed = new MessageEmbed()
                 .setAuthor(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: false }))
                 .setTitle('Help Window')
-                .setDescription('To get help on a specific module, type `k/help [module]` in the chat')
+                .setDescription('To get help on a specific module, type `k/help [command]` in the chat')
                 .addField('Bot Prefix', '`k/`')
-                .addField('Account Modules', '```md\n1. profile\n2. inventory\n3. skins\n4. collection\n5. status\n6. notification\n7. notifications```')
+                .addField('Account Modules', '```md\n1. profile\n2. inventory\n3. skins\n4. collection\n5. status\n6. notifications```')
                 .addField('Economy Modules', '```md\n1. balance\n2. beg\n3. bet\n4. bulkspin\n5. crime\n6. deposit\n7. give\n8. lb\n9. rob\n10. slots\n11. spin\n12. withdraw\n13. work```')
                 .addField('Market Modules', '```md\n1. shop\n2. collectables\n3. buy```')
                 .addField('Miscallaneous Modules', '```md\n1. infect\n2. cure\n3. daily```');
