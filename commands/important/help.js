9df5d79923912e41dde9b073d8595d1ff66d281f
EJ BEAN <danonymousarmy@gmail.com>
2021-04-29 21:40:45 +0530

    fix : typos

--- commands/important/help.js
+++ commands/important/help.js
@@ -13,7 +13,7 @@ module.exports = {
                 .addField('Account Modules', '```md\n1. profile\n2. inventory\n3. skins\n4. collection\n5. status\n6. notification\n7. notifications```')
                 .addField('Economy Modules', '```md\n1. balance\n2. beg\n3. bet\n4. bulkspin\n5. crime\n6. deposit\n7. give\n8. lb\n9. rob\n10. slots\n11. spin\n12. withdraw\n13. work```')
                 .addField('Market Modules', '```md\n1. shop\n2. collectables\n3. buy```')
-                .addField('Miscallaneou Modules', '```md\n1. infect\n2. cure\n3. daily```');
+                .addField('Miscallaneous Modules', '```md\n1. infect\n2. cure\n3. daily```');
             message.reply(embed);
             return;
         }
