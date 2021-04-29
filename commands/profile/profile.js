cb8555ee879d8e608fe10ee17a1d8654da3e0a50
EJ BEAN <danonymousarmy@gmail.com>
2021-04-29 15:33:07 +0530

    feat : profile

--- commands/profile/profile.js
+++ commands/profile/profile.js
@@ -4,7 +4,7 @@ const db = require('../../modules'),
     { MessageEmbed } = require('discord.js'),
     levels = require('../../mongo'),
     utils = require('../../modules/messageUtils'),
-    item = require('../../modules/utils');
+    items = require('../../data/items');
 module.exports = {
     name: 'userinfo',
     aliases: ['profile'],
@@ -14,7 +14,8 @@ module.exports = {
         if (!args[0]) user = message.author;
         else user = await message.client.users.fetch(args[0].replace(/\D/g, '')).catch(() => {});
         if (!user) return message.channel.send('Unknown user');
-        let devEmote, staffEmote, earlySupporter, activeItems;
+        // eslint-disable-next-line prefer-const
+        let devEmote, staffEmote, earlySupporter, activeItems = [];
         const krunkitis = await db.utils.krunkitis(user.id),
             premium = await db.utils.premium(user.id),
             verified = await db.utils.verified(user.id),
@@ -22,7 +23,7 @@ module.exports = {
             netWorth = parseInt(wallet + bank),
             xp = await levels.getXP(user.id),
             level = await levels.getLevel(user.id),
-            padlock = await item.findItem(user.id, 'padlock');
+            dupes = new Map();
             // eslint-disable-next-line no-unused-vars
         if (data.earlySupporter.includes(user.id)) earlySupporter = data.emotes.earlysupporter;
         // else earlySupporter = '';
@@ -30,11 +31,18 @@ module.exports = {
         else staffEmote = '';
         if (data.devs.includes(user.id)) devEmote = data.emotes.developer;
         else devEmote = '';
-        console.log(padlock);
-        if (padlock != undefined) activeItems = ':lock: Padlock';
-        else activeItems = 'Nothing to see here';
         const embedColor = utils.getEmbedColor(level),
             color = utils.parseEmbedColor(level);
+        const userItems = (await db.utils.itemInventory(user.id)).map(x => items.items[x])
+            .filter(x => {
+                const count = dupes.get(x.id) || 0;
+                dupes.set(x.id, count + 1);
+                return !count;
+            });
+        for (const item of userItems) {
+            const count = dupes.get(item.id);
+            activeItems.push(`${item.icon} ${item.name}${count == 1 ? '' : ` x ${count}`}`);
+        }
         const embed = new MessageEmbed()
             .setAuthor(`${user.username}`)
             .setTitle(`${krunkitis ? emotes.krunkitis : ''} ${premium ? emotes.premium : ''} ${verified ? emotes.verified : ''} ${staffEmote} ${devEmote}`.replace(/\s/g, ' '))
@@ -48,7 +56,7 @@ module.exports = {
                 { name: 'Wallet', value: `\`${wallet}\``, inline: true },
                 { name: 'Bank', value: `\`${bank}\``, inline: true },
                 { name: 'Net worth', value: `\`${netWorth}\``, inline: true },
-                { name: 'Active Items', value: `${activeItems}` },
+                { name: 'Active Items', value: `${activeItems.join('\n') || 'Nothing to see here'}` },
             );
         message.channel.send(embed);
     },
