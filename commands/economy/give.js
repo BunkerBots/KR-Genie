888a8c457b1e7aa6dc72e4b247ea44a1e1e0c276
EJ BEAN <danonymousarmy@gmail.com>
2021-04-30 13:18:25 +0530

    feat : embedification

--- commands/economy/give.js
+++ commands/economy/give.js
@@ -11,15 +11,15 @@ module.exports = {
     execute: async(message, args) => {
         if (!args[0]) return message.channel.send(`Who are you giving ${data.emotes.kr} to?`);
         const user = await message.guild.members.fetch(args[0].replace(/\D/g, '')).catch(() => {});
-        if (!user) return message.reply('No user found nerd..');
+        if (!user) return message.reply(await utils.createEmbed(message.author, 'RED', 'No user found nerd..'));
         const { wallet } = await db.utils.balance(message.author.id);
         const balance = await db.utils.balance(message.author.id);
-        if (!args[1]) await message.reply('You can\'t gift thing air you dumb');
-        if (user.id == message.author.id) return message.reply('Why are you gifting yourself?...');
+        if (!args[1]) await message.reply(await utils.createEmbed(message.author, 'RED', 'You can\'t gift thin air you dumb'));
+        if (user.id == message.author.id) return message.reply(await utils.createEmbed(message.author, 'RED', 'Why are you gifting yourself?...'));
         const krtogive = parseInt(utils.parse(args[1], balance));
-        if (wallet <= 0) return message.channel.send(`You do not have enough ${data.emotes.kr}`);
-        if (wallet < krtogive) return message.channel.send(`You do not have ${data.emotes.kr}${krtogive}`);
-        if (!Number.isInteger(krtogive)) return message.channel.send('What are you doing? That\'s not even a valid number');
+        if (wallet <= 0) return message.channel.send(await utils.createEmbed(message.author, 'RED', `You don't have any ${data.emotes.kr} in your wallet`));
+        if (wallet < krtogive) return message.channel.send(await utils.createEmbed(message.author, 'RED', `You do not have ${data.emotes.kr}${krtogive} in your wallet`));
+        if (!Number.isInteger(krtogive)) return message.channel.send(await utils.createEmbed(message.author, 'RED', 'What are you doing? That\'s not even a valid number'));
         await db.utils.addKR(user.id, krtogive);
         await db.utils.addKR(message.author.id, -krtogive);
         const authorbal = await db.utils.balance(message.author.id);
