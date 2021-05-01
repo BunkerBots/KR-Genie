4960df455130c11e00c094ecda75ea6679350c44
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-05-01 16:38:03 +0530

    Fix: private git module installation in heroky
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- heroku-prebuild.sh
+++ heroku-prebuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ "$GITHUB_SSH_KEY" != "" ]; then
-    echo "Detected GITHUB_SSH_KEY. Setting git config to use the security token" >&1
-    git config --global url.ssh://git@github.com/.insteadOf https://github.com/
+if [ "$GITHUB_TOKEN" != "" ]; then
+    echo "Detected GITHUB_TOKEN. Setting git config to use the security token" >&1
+    echo "https://$GITHUB_TOKEN@github.com" > ~/.git-credentials
+    git config --global credential.helper store
 fi
