2884da7418d211b41b2ed18472857bf2f90a0eff
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-05-01 16:18:25 +0530

    Chore: heroku ssh
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- heroku-prebuild.sh
+++ heroku-prebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$GITHUB_TOKEN" != "" ]; then
-    echo "Detected GITHUB_TOKEN. Setting git config to use the security token" >&1
-    git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf git@github.com:
+if [ "$GITHUB_SSH_KEY" != "" ]; then
+    echo "Detected GITHUB_SSH_KEY. Setting git config to use the security token" >&1
+    git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 fi
