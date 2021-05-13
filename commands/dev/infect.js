53136ace9292bc6714bd3bbed6028f69c996c5e6
EJ BEAN <danonymousarmy@gmail.com>
2021-05-13 15:17:03 +0530

    dependencies fix

--- commands/dev/infect.js
+++ commands/dev/infect.js
@@ -1,5 +1,5 @@
 const db = require('../../modules');
-const { devs, staff } = require('../../data');
+const { devs, staff, emotes } = require('../../data');
 module.exports = {
     name: 'devinfect',
     dev: true,
@@ -8,6 +8,6 @@ module.exports = {
         const target = await message.client.users.fetch(args[0].replace(/\D/g, '')).catch(() => {});
         if (!target) return message.channel.send('Unknown user');
         await db.utils.infect(target.id);
-        message.channel.send(`Successfully infected \`${target.username}\` ${data.emotes.krunkitis}`);
+        message.channel.send(`Successfully infected \`${target.username}\` ${emotes.krunkitis}`);
     },
 };
