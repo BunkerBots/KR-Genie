69437f51ec74048dd0d69dc40926efd1465ccbe1
EJ BEAN <danonymousarmy@gmail.com>
2021-05-13 15:03:26 +0530

    perms for dev infect

--- commands/dev/infect.js
+++ commands/dev/infect.js
@@ -1,10 +1,10 @@
 const db = require('../../modules');
-const data = require('../../data');
+const { devs, staff } = require('../../data');
 module.exports = {
     name: 'devinfect',
     dev: true,
     execute: async(message, args) => {
-        if (!data.devs.includes(message.author.id)) return;
+        if (!(devs.includes(message.author.id) || staff.includes(message.author.id))) return;
         const target = await message.client.users.fetch(args[0].replace(/\D/g, '')).catch(() => {});
         if (!target) return message.channel.send('Unknown user');
         await db.utils.infect(target.id);
