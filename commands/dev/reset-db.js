438ac791fec57e9b501c24176aa1ac90f2b0b6a9
EJ BEAN <danonymousarmy@gmail.com>
2021-05-18 13:16:20 +0530

    feat : added backup before clearing db

--- commands/dev/reset-db.js
+++ commands/dev/reset-db.js
@@ -8,8 +8,9 @@ module.exports = {
     dev: true,
     execute: async(message) => {
         if (!devs.includes(message.author.id)) return;
+        await db.backup();
         await db.clear();
         message.channel.send('Successfully reset the database');
-        logger.commandsLog(message.author, 'resedb', `**${message.author.tag}** (dev) reset DataBase`, message.guild, 'Reset: DataBase}');
+        logger.commandsLog(message.author, 'resetdb', `**${message.author.tag}** (dev) reset DataBase`, message.guild, 'Reset: DataBase}');
     },
 };
