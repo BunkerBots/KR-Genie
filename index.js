782812f0d503915845ae1a1c99c52fc05a685a98
JJ-G4M3R <30411592+JJ-G4M3R@users.noreply.github.com>
2021-05-19 22:04:14 -0700

    Finished cleaning up index.js

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const { Client, Collection, MessageEmbed, Intents } = require('discord.js'),
     intents = (new Intents).add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_EMOJIS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS),
     logger = require('./modules/logger.js');
 
-// Login
-require('dotenv').config();
-bot.login(process.env.NODE_ENV == 'PRODUCTION' ? process.env.TOKEN : process.env.TEST_TOKEN);
+let maintenance = false;
 
 // Load commands
 bot.commands = new Collection();
@@ -23,12 +21,16 @@ for (const folder of commandFolders) {
     }
 }
 
-// ready
+// Login
+require('dotenv').config();
+bot.login(process.env.NODE_ENV == 'PRODUCTION' ? process.env.TOKEN : process.env.TEST_TOKEN);
+
+// Ready
 bot.on('ready', async() => {
     module.exports.bot = bot;
 
     logger.debug('index.js', 'Logging in');
-    bot.user.setPresence({ activity: { name: 'KR fly by', type: 'WATCHING' }, status: 'idle', });
+    bot.user.setPresence({ activity: { name: 'KR fly by', type: 'WATCHING' }, status: 'online', });
     logger.info('Ready!');
 
     require('./modules/messageUtils').load(bot);
@@ -57,87 +59,61 @@ bot.on('ready', async() => {
     cron.schedule('30 14 * * *', () => { db.backup(await bot.channels.resolve(data.id.channels['backup-dump'])) }); // Every day at 2:30 PM
 });
 
-let maintanence = false;
+// onMessage Event
 bot.on('message', async message => {
-    const args = message.content.substring(core.prefix.length).split(' ');
-    const cmd = args.shift().toLowerCase();
-    switch (cmd) {
-        case 'maintenance':
-            if (!data.devs.includes(message.author.id)) return;
-            if (!args[0]) return;
-            if (args[0] === 'on') {
-                maintanence = true;
-                bot.user.setPresence({
-                    activity: {
-                        name: 'Maintenance mode',
-                        type: 'PLAYING',
-                    },
-                    status: 'dnd',
-                });
-            } else {
-                maintanence = false;
-                bot.user.setPresence({
-                    activity: {
-                        name: 'KR fly by',
-                        type: 'WATCHING',
-                    },
-                    status: 'idle',
-                });
-            }
-            message.channel.send(`maintenance mode ${maintanence ? 'enabled' : 'disabled'}`);
-            break;
-    }
-});
+    /** Ignores:
+     * - Bots
+     * - Messages that don't start with bot prefix
+     * - Banned users */
+    if (message.author.bot || !message.content.toLowerCase().startsWith(core.prefix) || await db.utils.banned(message.author.id)) return;
 
-bot.on('message', async message => {
-    if (message.author.bot) return;
-    if (!message.content.toLowerCase().startsWith(core.prefix)) return;
-    // check if the user is banned
-    const banned = await db.utils.banned(message.author.id);
-    if (banned == true) return;
+    // Maintenance mode
+    if (data.devs.includes(message.author.id) && message.content.startsWith(`${core.prefix}maintenance`) && message.content.split.length == 2) {
+        maintenance = message.content.split(' ')[1] == 'on' ? true : false;
+        if (maintenance) bot.user.setPresence({ activity: { name: 'the janitor', type: 'WATCHING' }, status: 'dnd' });
+        else bot.user.setPresence({ activity: { name: 'KR fly by', type: 'WATCHING' }, status: 'online' });
+        message.channel.send(`maintenance mode ${maintenance ? 'enabled' : 'disabled'}`);
+    }
+
     const args = message.content.substring(core.prefix.length).trim().split(' '),
-        commandName = args.shift().toLowerCase();
-    const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+        commandName = args.shift().toLowerCase(),
+        command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
     if (!command) return;
-    if (!cooldowns.has(command.name))
-        cooldowns.set(command.name, new Collection());
+    if (!cooldowns.has(command.name)) cooldowns.set(command.name, new Collection());
 
+    const now = Date.now(),
+        timestamps = cooldowns.get(command.name),
+        expirationTime = timestamps.get(message.author.id) + ((command.cooldown || 0) * 1000);
 
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 0) * 1000;
+    if (!data.devs.includes(message.author.id) && timestamps.has(message.author.id)) {
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000,
+                time = timeLeft / 60;
+            let seconds;
 
-    if (!data.devs.includes(message.author.id)) {
-        if (timestamps.has(message.author.id)) {
-            const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+            if (time.toFixed(0) < 1) seconds = `${timeLeft.toFixed(1)} second(s)`;
+            else seconds = `${time.toFixed(1)} minute(s)`;
 
-            if (now < expirationTime) {
-                const timeLeft = (expirationTime - now) / 1000;
-                const time = timeLeft / 60;
-                let seconds;
-                if (time.toFixed(0) < 1) seconds = `${timeLeft.toFixed(1)} second(s)`;
-                else seconds = `${time.toFixed(1)} minute(s)`;
-                return message.reply(new MessageEmbed()
-                    .setColor('YELLOW')
-                    .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: false }))
-                    .setTitle('Whoa whoa hold on...')
-                    .setDescription(`You need to wait \`${seconds}\` before reusing the \`${command.name}\` command.`)
-                    .setFooter('notstonks4u'));
-            } else timestamps.delete(message.author.id);
-        }
+            return message.reply(new MessageEmbed()
+                .setColor('YELLOW')
+                .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: false }))
+                .setTitle('Whoa whoa hold on...')
+                .setDescription(`You need to wait \`${seconds}\` before reusing the \`${command.name}\` command.`)
+                .setFooter('#no-stonks4u'));
+        } else timestamps.delete(message.author.id);
     }
 
-    if (maintanence === false) {
+    if (!maintenance) {
         try {
             message.timestamps = timestamps;
             command.execute(message, args, bot);
-            // if (xpCommands.includes(command.name.toLowerCase())) levels.addXP(message.author.id, 23, message);
             if (!command.manualStamp) timestamps.set(message.author.id, now);
-        } catch (error) {
-            console.log(error);
-        }
+        } catch (error) { console.log(error) }
     } else {
         message.channel.send(new MessageEmbed()
-            .setDescription('```diff\n- The bot commands are disabled for maintenance , please try again later``` \n<a:tools:830536514303295518> [Join our support server](https://discord.gg/DfhQDQ8e8c)').setColor('BLACK').setURL('https://discord.gg/DfhQDQ8e8c')).catch(e => console.log(e));
+            .setDescription('```diff\n- The bot commands are disabled for maintenance , please try again later``` \n<a:tools:830536514303295518> [Join our support server](https://discord.gg/DfhQDQ8e8c)')
+            .setColor('BLACK')
+            .setURL('https://discord.gg/DfhQDQ8e8c')
+        ).catch(e => console.log(e));
     }
 });
