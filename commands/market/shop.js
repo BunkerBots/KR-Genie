19933986e135d0603edaa159ab2b626e91da55bd
EJ BEAN <danonymousarmy@gmail.com>
2021-05-23 16:46:11 +0530

    fix : imports

--- commands/market/shop.js
+++ commands/market/shop.js
@@ -1,6 +1,6 @@
 import { MessageEmbed } from 'discord.js';
 import { emotes } from '../../data/index.js';
-import items from '../../data/items.js';
+import * as items from '../../data/items.js';
 import comma from '../../modules/comma.js';
 import * as core from '../../data/JSON/core.json';
 
