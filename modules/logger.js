369f7255b153372a375bcf7a5df9368b8a6620c7
EJ BEAN <danonymousarmy@gmail.com>
2021-06-24 18:35:05 +0530

    logger fix

--- modules/logger.js
+++ modules/logger.js
@@ -41,28 +41,30 @@ function unhandledError(e) {
     error('Unhandled Error', util.inspect(e));
 }
 
-let commandsLog = () => {};
+let commandsLogChannel;
+let KBlogs;
 
 async function init(bot) {
-    const commandsLogChannel = await bot.channels.fetch(id.channels['commands-log']);
-    const KBlogs = process.env.NODE_ENV == 'PRODUCTION' ? await bot.channels.fetch(id.channels['kb-commands-log']) : undefined;
+    commandsLogChannel = await bot.channels.fetch(id.channels['commands-log']);
+    KBlogs = process.env.NODE_ENV == 'PRODUCTION' ? await bot.channels.fetch(id.channels['kb-commands-log']) : undefined;
     LogChannel = await bot.channels.fetch(id.channels['crash-logs']);
     error = (functionName = ' ', errorMessage = ' ') => {
         console.error(`!!! ${functionName} | ${errorMessage} !!!`.red);
         return LogChannel.send(`**${functionName}**\n\`\`\`js\n${errorMessage}\`\`\` `);
     };
-    commandsLog = async(user, commandName, comment, guild, args, type) => {
-        const embed = new MessageEmbed()
-            .setAuthor(user.tag, user.displayAvatarURL({ dynamic: true }))
-            .setTitle(commandName)
-            .setColor(core.embed)
-            .setDescription(`${comment || ''}\`\`\`yaml\nGuild: ${guild.name || ''}\nArguments: ${args || 'null'}\n${type || 0}\`\`\``)
-            .setTimestamp();
-        commandsLogChannel.send(embed);
-        KBlogs?.send(embed);
-    };
     return true;
 }
+
+const commandsLog = async(user, commandName, comment, guild, args, type) => {
+    const embed = new MessageEmbed()
+        .setAuthor(user.tag, user.displayAvatarURL({ dynamic: true }))
+        .setTitle(commandName)
+        .setColor(core.embed)
+        .setDescription(`${comment || ''}\`\`\`yaml\nGuild: ${guild.name || ''}\nArguments: ${args || 'null'}\n${type || 0}\`\`\``)
+        .setTimestamp();
+    commandsLogChannel?.send(embed);
+    KBlogs?.send(embed);
+};
 
 export default { error, commandsLog, info, unhandledError, debug, init };
 export { error, commandsLog, info, unhandledError, debug, init };
