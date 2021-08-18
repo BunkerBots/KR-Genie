8988a374dbe5733ee3bbc3f03f016704e1b6448b
EJBEAN2op <danonymousarmy@gmail.com>
2021-08-18 16:24:31 +0530

    refactor : optional mentions #10

--- commands/important/change-log.js
+++ commands/important/change-log.js
@@ -22,7 +22,7 @@ export default {
             const btns = [
                 new MessageButton().setStyle('LINK').setURL(`${core.git['KR-genie']}`).setLabel('Latest Source')
             ];
-            message.reply({ embeds: [msgembed], components: [new MessageActionRow().addComponents(...btns)] });
+            message.reply({ embeds: [msgembed], components: [new MessageActionRow().addComponents(...btns)], allowedMentions: { repliedUser: false } });
         });
 
         async function processLineByLine() {
