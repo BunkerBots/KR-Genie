4451a6f38fffb4b72e3a95dffe0cf58b238b4ca3
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-08-23 17:23:12 +0530

    fix ur stuff ej
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

---
+++ modules/embedPages.js
@@ -0,0 +1,119 @@
+import { Message, MessageEmbed } from 'discord.js';
+import core from '../data/JSON/core.json';
+// import { table } from 'table';
+// import utils from './messageUtils.js';
+
+class StaticEmbeds extends Message {
+
+    /**
+     * Embed pages generator (static)
+     * @param {Array} arr
+     * @param {*} user
+     * @param {Array} args
+     * @param {Number} startIndex
+     */
+    constructor(message, arr, user, args, description) {
+        super();
+        this.message = message;
+        this.arr = arr;
+        this.user = user;
+        this.footer = '';
+        this.pageNumber = '';
+        this.args = args;
+        this.description = description;
+    }
+
+    generateEmbed(type, description) {
+        const generateEmbed = (start) => {
+            const current = this.arr.slice(start, start + 10);
+            const embed = new MessageEmbed()
+                .setAuthor(`Requested by ${this.message.author.username}`, this.message.author.displayAvatarURL({ dynamic: true }))
+                .setTitle(this.user ? `${this.user.username}'s ${type}` : `${type}`)
+                .setColor(core.embed)
+                .setDescription(this.description ? description : `${description} ${start + 1}-${start + current.length} out of ${this.arr.length}`)
+                .setFooter(this.footer);
+            current.forEach(g => embed.addField('\u200b', g));
+            return embed;
+        };
+        if (this.arr.length < 10) {
+            this.footer = '1 out of 1';
+            this.message.channel.send(generateEmbed(0));
+            return;
+        }
+
+        const page = this.args.shift();
+        if (!page) {
+            const lastPage = Math.ceil(this.arr.length / 10);
+            this.footer = `1 out of ${lastPage}`;
+            this.message.channel.send(generateEmbed(0));
+        } else {
+            const lastPage = Math.ceil(this.arr.length / 10);
+            this.footer = `${page} out of ${lastPage}`;
+            this.pageNumber = page - 1;
+            const currentindex = parseInt(this.pageNumber * 10);
+            console.log(currentindex);
+            if (currentindex > this.arr.length) return;
+            this.message.channel.send(generateEmbed(currentindex));
+        }
+    }
+
+    // async tableParser() {
+    //     const sortByCash = this.message.content.includes('--cash');
+    //     const generateEmbed = async(start) => {
+    //         const current = this.arr.slice(start, start + 10);
+    //         const embed = new MessageEmbed()
+    //             .setAuthor(`Requested by ${this.message.author.username}`, this.message.author.displayAvatarURL({ dynamic: true }))
+    //             // .setTitle(this.user ? `${this.user.username}'s ${type}` : `${type}`)
+    //             // .setDescription(`${description} ${start + 1}-${start + current.length} out of ${this.arr.length}`)
+    //             .setFooter(this.footer);
+    //         const lbUsers = [];
+    //         for (const i of current) {
+    //             console.log('loop');
+    //             const index = current.indexOf(i);
+    //             const bankBal = i.balance.wallet + (sortByCash ? 0 : i.balance.bank);
+    //             const user = await utils.getID(i.id);
+    //             lbUsers.push([index + 1, `${user.tag}`, `${bankBal}`]);
+    //         }
+    //         lbUsers.unshift(['', 'KR Leaderboard', '']);
+    //         const dat = table(lbUsers);
+    //         embed.setDescription(`\`\`\`${dat}\`\`\``);
+    //         console.log(dat);
+
+    //         return embed;
+    //     };
+    //     if (this.arr.length < 10) {
+    //         this.footer = '1 out of 1';
+    //         this.message.channel.send('Generating tables').then(async msg => {
+    //             this.message.channel.send(await generateEmbed(0));
+    //             msg.delete();
+    //         });
+    //         return;
+    //     }
+
+    //     const page = this.args.shift();
+    //     if (!page) {
+    //         const lastPage = Math.ceil(this.arr.length / 10);
+    //         this.footer = `1 out of ${lastPage}`;
+    //         this.message.channel.send('Generating tables').then(async msg => {
+    //             this.message.channel.send(await generateEmbed(0));
+    //             msg.delete();
+    //         });
+    //     } else {
+    //         const lastPage = Math.ceil(this.arr.length / 10);
+    //         this.footer = `${page} out of ${lastPage}`;
+    //         this.pageNumber = page - 1;
+    //         const currentindex = parseInt(this.pageNumber * 10);
+    //         console.log(currentindex);
+    //         if (currentindex > this.arr.length) return;
+    //         this.message.channel.send('Generating tables').then(async msg => {
+    //             this.message.channel.send(await generateEmbed(currentindex));
+    //             msg.delete();
+    //         });
+    //     }
+    // }
+
+}
+
+Message.prototype.staticEmbeds = StaticEmbeds;
+export default StaticEmbeds;
+
