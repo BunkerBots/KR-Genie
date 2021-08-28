a8eb2b94097b4141943a6bc2b4f19bdde51f45fd
EJBEAN2op <danonymousarmy@gmail.com>
2021-08-28 16:00:13 +0530

    feat : added cache for faster page generation

---
+++ modules/Cache.js
@@ -0,0 +1,60 @@
+class Cache {
+
+    constructor() {
+        this.cache = new Object();
+    }
+
+    /**
+     * Create a new Array
+     * @param {String} identifier String to be used as a key for cache generation
+     * @returns Array
+     */
+    create(identifier) {
+        this.cache[identifier] = [];
+        console.log('Successfully created cache with identifier ' + identifier);
+        return this.cache[identifier];
+    }
+
+    add(identifier, value) {
+        const val = this.cache[identifier];
+        if (!val) throw new Error(`Property ${identifier} does not exist in cache`);
+        if (value instanceof Array) this.cache[identifier] = [...val, ...value];
+        else val.push(value);
+        return this.cache[identifier];
+    }
+
+    get(identifier) {
+        const val = this.cache[identifier];
+        if (!val) throw new Error(`Property ${identifier} does not exist in cache`);
+        return val;
+    }
+
+    delete(identifier, value) {
+        const val = this.cache[identifier];
+        if (!val) throw new Error(`Property ${identifier} does not exist in cache`);
+        if (value instanceof Array) {
+            for (const x of value) {
+                const index = val.indexOf(x);
+                val.splice(index, 1);
+            }
+            return val;
+        } else {
+            const index = val.indexOf(value);
+            if (index === -1) throw new Error(`${value} does not exist in ${val}`);
+            val.splice(index, 1);
+            return val;
+        }
+    }
+
+    /**
+     * Returns an Array with first element as the cache tree and second element as the cache element count
+     * @returns Array
+     */
+    tree() {
+        return [this.cache, Object.keys(this.cache).length];
+    }
+
+}
+
+
+export default Cache;
