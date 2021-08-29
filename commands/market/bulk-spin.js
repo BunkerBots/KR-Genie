d51a36bee45d9bb68400a6946abe3053fd692682
EJBEAN2op <danonymousarmy@gmail.com>
2021-08-29 22:29:23 +0530

    refactor : v13 spins w/ buttons integration

--- commands/market/bulk-spin.js
+++ commands/market/bulk-spin.js
@@ -1,12 +1,29 @@
 import skinfetcher from '../../modules/skins.js';
 import dat from '../../data/index.js';
-import { MessageEmbed, MessageButton, MessageActionRow } from 'discord.js';
+import { MessageActionRow, MessageEmbed, MessageSelectMenu, MessageButton } from 'discord.js';
 import db from '../../modules/db/economy.js';
 import utils from '../../modules/utils.js';
 import { createEmbed } from '../../modules/messageUtils.js';
 import { addXP } from '../../modules/db/levels.js';
 import core from '../../data/JSON/core.json';
+import { starterSpin, eliteSpin } from '../../modules/spins.js';
 const { emotes } = dat;
+
+const menuOptions = [{
+    label: 'Heroic',
+    description: 'Heroic spin',
+    value: 'heroic',
+},
+{
+    label: 'Elite',
+    description: 'Elite spin',
+    value: 'elite',
+},
+{
+    label: 'Starter',
+    description: 'Starter spin',
+    value: 'starter',
+}];
 
 
 export default {
@@ -22,14 +39,58 @@ export default {
         const verified = await db.utils.verified(message.author.id);
         if (verified == true) limit = 15;
         if (premium == true) limit = 20;
-        const spinarr = [];
-        const rarityarr = [];
+
         if (!args[0]) return message.reply(createEmbed(message.author, 'RED', 'How many spins are you gonna do..'));
-        if (Number.isInteger(parseInt(args[0]))) {
+        if (Number.isInteger(parseInt(args[0])) && args.length < 2) {
+            const menuEmbed = new MessageEmbed()
+                .setAuthor(`${message.author.username}`, message.author.avatarURL({ dynamic: true }))
+                .setDescription('Please select an appropriate category from the menu given below')
+                .setColor(core.embed)
+                .setTimestamp();
+
+
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageSelectMenu()
+                        .setCustomId('inventory')
+                        .setMaxValues(1)
+                        .setPlaceholder('Select a category')
+                        .addOptions(menuOptions),
+                );
+
+            const menu = await message.reply({ components: [row], embeds: [menuEmbed] });
+
+            const filter = i => i.user.id === message.author.id;
+            const collector = message.channel.createMessageComponentCollector({ filter, componentType: 'SELECT_MENU', time: 60000 });
+
+            collector.on('collect', async i => {
+            // if (i.user.id !== message.author.id) return i.reply({ content: 'These buttons aren\'t for you!', ephemeral: true });
+                Spin(i.values[0]);
+                // else if (i.values[0] == 'tools') toolsCmd.execute(message, args);
+                // else if (i.values[0] == 'collectables') collectablesCmd.execute(message, args);
+                menu.delete();
+                collector.stop();
+            });
+
+            collector.on('end', () => console.log('timer end'));
+        } else if (Number.isInteger(parseInt(args[0])) && args[1]) {
+            if (!['heroic', 'starter', 'elite'].includes(args[1].toLowerCase())) return message.reply(createEmbed(message.author, 'RED', 'That\'s not a valid spin mate'));
+            Spin(args[1].toLowerCase());
+        } else
+            return message.reply(createEmbed(message.author, 'RED', `Expected a number and gave me some random \`${args.join(' ')}\``));
+
+
+        async function Spin(spin) {
+            let amount, func, type;
+            if (spin == 'heroic') amount = 500, func = utils.getRandomRaritySkin, type = 'Heroic';
+            else if (spin == 'starter') amount = 50, func = starterSpin, type = 'Starter';
+            else if (spin == 'elite') amount = 150, func = eliteSpin, type = 'Elite';
+            const spinarr = [];
+            const rarityarr = [];
             if (parseInt(args[0]) > parseInt(limit)) return message.channel.send(createEmbed(message.author, 'RED', `You can only do ${limit} bulk spins per use`));
             if (parseInt(args[0]) < 0) return message.channel.send(createEmbed(message.author, 'RED', `${parseInt(args[0])} Skins were removed from your inventory at random!\n:)`));
             if (parseInt(args[0]) == 1 || parseInt(args[0]) == 0) return message.channel.send(createEmbed(message.author, 'RED', 'JUST DO SPIN!!\nSMH...'));
-            const KR = parseInt(500 * parseInt(args[0]));
+            const KR = parseInt(amount * parseInt(args[0]));
             const { wallet } = await db.utils.balance(message.author.id);
             let recommended;
             const roundedval = parseInt(wallet / 500).toFixed(0);
@@ -38,7 +99,7 @@ export default {
             if (KR > wallet) {
                 message.reply({ embeds: [new MessageEmbed()
                     .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
-                    .setDescription(`You do not have enough ${dat.emotes.kr} to do ${parseInt(args[0])} spins\n\`Recommended: ${recommended}\``)] });
+                    .setDescription(`You do not have enough ${dat.emotes.kr} to do ${parseInt(args[0])} ${type} spins\n\`Recommended: ${recommended}\``)] });
                 return;
             }
             message.channel.send({ embeds: [new MessageEmbed()
@@ -47,7 +108,7 @@ export default {
                     const skinToPush = [];
                     const itemToPush = [];
                     for (let i = 0; i < parseInt(args[0]); i++) {
-                        const randomSkin = utils.getRandomRaritySkin();
+                        const randomSkin = func();
                         if (randomSkin.index == 1659) itemToPush.push(3);
                         else if (randomSkin.index == 944) itemToPush.push(2);
                         else skinToPush.push(randomSkin.index);
@@ -63,9 +124,9 @@ export default {
 
                     const minimizedEmbed = mapRarity(rarityarr)
                         .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
-                        .setTitle(`${parseInt(args[0])} Heroic Spins Result`)
+                        .setTitle(`${parseInt(args[0])} ${type} Spins Result`)
                         .setColor(core.embed)
-                        .addField('\u200b', 'React with `↕️` to view the skin names');
+                        .addField('\u200b', 'Press the button to view the skin names');
                     const maximizeRow = new MessageActionRow()
                         .addComponents(new MessageButton().setLabel('Maximize').setStyle('SUCCESS').setCustomId('maximize'));
                     const minimizeRow = new MessageActionRow()
@@ -98,8 +159,7 @@ export default {
                     addXP(message.author.id, 23, message);
                     //                     if (!dat.staff.includes(message.author.id)) message.timestamps.set(message.author.id, Date.now());
                 });
-        } else
-            return message.reply(createEmbed(message.author, 'RED', `Expected a number and gave me some random \`${args.join(' ')}\``));
+        }
     },
 };
 
@@ -114,3 +174,4 @@ function mapRarity(rarityArr) {
         if (reversedArr[x].length !== 0) embed.addField(`${res[x][0]} ${res[x][1]}`, `${reversedArr[x].length || 0}`);
     return embed;
 }
+
