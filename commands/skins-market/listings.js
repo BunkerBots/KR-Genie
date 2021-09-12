bc99d6919d6b6c95b50c7c219979306cad710237
EJBEAN2op <danonymousarmy@gmail.com>
2021-09-12 15:57:04 +0530

    feat : paginator

--- commands/skins-market/listings.js
+++ commands/skins-market/listings.js
@@ -3,7 +3,9 @@ import Skins from '../../modules/skins.js';
 import { emotes, core } from '../../data/index.js';
 import marketDB from '../../modules/db/market.js';
 import comma from '../../modules/comma.js';
-
+import utils from '../../modules/messageUtils.js';
+// import Paginator from '../../modules/paginate.js';
+import Paginator from '../../modules/paginator.js';
 
 export default {
     name: 'listings',
@@ -11,47 +13,41 @@ export default {
     cooldown: 10,
     description: 'Get a list of all skin listings globally',
     expectedArgs: 'k/listings',
-    execute: async(message, args) => {
-        // if (!(devs.includes(message.author.id) || staff.includes(message.author.id) || testers.includes(message.author.id))) return;
+    execute: async(message, args, bot) => {
+        const msg = await message.channel.send({ embeds: [new MessageEmbed()
+            .setDescription(`${emotes.loading} Loading global listings...`)] });
         const listing = await marketDB.utils.getListing(1);
-        console.log(listing);
         const sortedArr = listing.sort((a, b) => a.price - b.price).reverse();
-        let pageNumber, footer;
-        /**
-         * Creates an embed with skinsarr starting from an index.
-         * @param {number} start The index to start from.
-         */
-        const generateEmbed = start => {
-            const current = sortedArr.slice(start, start + 10);
+        const values = [...sortedArr];
+        const max = Math.ceil(values.length / 10);
+        const tagsCache = global.cache.get('tags');
+        const options = { author: message.author, current: 1, maxValues: values.length, max: max };
+        // thiss is an arr ['collectable name']
+        const paginator = new Paginator(bot, message.channel, options, async(index, dat) => {
+            let tag;
+            const res = [];
+            for (const i of sortedArr.slice(index, index + 10)) {
+                const usertag = tagsCache.find(x => x.id == i.id);
+                if (usertag) {
+                    tag = usertag.tag;
+                    res.push({ name: tag, skin: Skins.emoteColorParse(i.rarity), skinName: i.name, id: i.id, price: i.price });
+                } else {
+                    const user = await utils.getID(i.userID);
+                    res.push({ name: user.tag, skin: Skins.emoteColorParse(i.rarity), skinNname: i.name, id: i.id, price: i.price });
+                    global.cache.set('tags', { id: i.userID, tag: user.tag });
+                }
+            }
             const embed = new MessageEmbed()
-                .setAuthor(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
-                .setTitle('Global Skins Listing')
+                .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
+                .setTitle('Global Levels leaderboard')
                 .setColor(core.embed)
-                .setDescription(`Showing skins ${start + 1}-${start + current.length} out of ${sortedArr.length}`)
-                .setFooter(footer);
-            current.forEach(g => embed.addField(`${Skins.emoteColorParse(g.rarity)} ${g.name} - ${emotes.kr}${comma(g.price)}`, `Item ID : \`${g.id}\` \nListed by <@${g.userID}>`));
-            return embed;
-        };
-        if (sortedArr.length < 10) {
-            footer = '1 out of 1';
-            message.channel.send(generateEmbed(0));
-            return;
-        }
-
-        const page = args.shift();
-        if (!page) {
-            const lastPage = Math.ceil(sortedArr.length / 10);
-            footer = `1 out of ${lastPage}`;
-            message.channel.send(generateEmbed(0));
-        } else {
-            const lastPage = Math.ceil(sortedArr.length / 10);
-            footer = `${page} out of ${lastPage}`;
-            pageNumber = page - 1;
-            const currentindex = parseInt(pageNumber * 10);
-            console.log(currentindex);
-            if (currentindex > sortedArr.length) return;
-            message.channel.send(generateEmbed(currentindex));
-        }
+                .setFooter(`${dat.page} out of ${max}`);
+            for (const i of res) embed.addField(`${i.skin} ${i.skinNname} - ${emotes.kr}${comma(i.price)}`, `Item ID : \`${i.id}\` \nListed by ${i.name}`);
+            const obj = { embeds: [embed] };
+            return obj;
+        });
+        await paginator.start();
+        msg.delete();
     },
 };
 
