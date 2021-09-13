4b5ea0d472cf7fa23a1303c7e585827090e7ed76
EJBEAN2op <danonymousarmy@gmail.com>
2021-09-13 16:00:28 +0530

    feat : error handling

--- commands/important/vote.js
+++ commands/important/vote.js
@@ -4,6 +4,7 @@ import fetch from 'node-fetch';
 import { core, emotes } from '../../data/index.js';
 const ku = '616588768358694913';
 import { config } from 'dotenv';
+import { createEmbed } from '../../modules/messageUtils.js';
 
 config();
 
@@ -13,12 +14,13 @@ export default {
     cooldown: 5,
     description: 'Shows the latest updates made to the bot',
     expectedArgs: 'k/info',
+    // eslint-disable-next-line no-unused-vars
     execute: async(message, _, bot) => {
         const url = `https://top.gg/api/bots/${ku}/check?userId=${message.author.id}`;
-        const res = await fetch(url, { method: 'GET', headers: { Authorization: process.env.TOPGG_TOKEN } });
+        const res = await fetch(url, { method: 'GET', headers: { Authorization: process.env.TOPGG_TOKEN } }).catch(() => {});
         const text = await res.text();
         const isVoted = JSON.parse(text).voted;
-        console.log(isVoted);
+        if (isVoted == undefined) return message.reply(createEmbed(message.author, 'RED', 'An error occured while creating an HTTP request, please DM a developer if the issue persists'));
         if (isVoted !== 1) {
             const voteEmbed = new MessageEmbed()
                 .setColor(core.embed)
