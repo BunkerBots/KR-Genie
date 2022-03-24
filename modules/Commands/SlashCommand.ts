1729a430e1636e8ae9ae98f4481efb3bd6084e56
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-24 14:16:29 +0530

    feat(*): integrate slash command builder

--- modules/Commands/SlashCommand.ts
+++ modules/Commands/SlashCommand.ts
@@ -1,8 +1,10 @@
-import { ChatInputApplicationCommandData, ApplicationCommand, ApplicationCommandData, Formatters, Permissions, CommandInteraction } from 'discord.js';
+import { ChatInputApplicationCommandData, ApplicationCommand, ApplicationCommandData, Formatters, Permissions, CommandInteraction, ApplicationCommandDataResolvable } from 'discord.js';
 import type { SlashCommand as ISC, SlashCommandPayload } from '../../types/Command';
 import type { Client } from '../../types/Client';
 import Command from './Command';
 import CommandTypes from './CommandTypes';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
 
 interface SlashCommand extends ISC { };
 
@@ -10,6 +12,7 @@ class SlashCommand extends Command {
     constructor(options: SlashCommandPayload) {
         super(options);
         this.execute = options.execute;
+        this.data = options.data.toJSON();
         this.type = CommandTypes.SLASH;
     }
 
@@ -26,57 +29,73 @@ class SlashCommand extends Command {
         await this.execute(interaction);
     }
 
-    serialize() {
-        return [{
-            name: this.name,
-            description: this.description
-        }];
+    static async sync(client: Client, commands: ISC['data'][], options: { guildId: string | null }) {
+        const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+        try {
+            console.log('Started refreshing application (/) commands.');
+    
+            await rest.put(
+                options.guildId ? 
+                Routes.applicationGuildCommands(client.user.id, options.guildId) :
+                Routes.applicationCommands(client.user.id),
+                { body: commands },
+            );
+    
+            console.log('Successfully reloaded application (/) commands.');
+        } catch (error) {
+            console.error(error);
+        }
+
     }
 
-    static async sync(client: Client, commands: ChatInputApplicationCommandData[], options: { guildId: string }) {
-        console.log('at sync')
-        // const ready = client.readyAt ? Promise.resolve() : new Promise(resolve => client.once('ready', resolve));
-        // await ready;
-        const currentCommands = options.guildId ? await client.application!.commands.fetch({
-            guildId: options.guildId,
-        }) : await client.application!.commands.fetch();
+    // static async sync(client: Client, commands: ISC['data'][], options: { guildId: string | null }) {
+    //     console.log('at sync')
+    //     // const ready = client.readyAt ? Promise.resolve() : new Promise(resolve => client.once('ready', resolve));
+    //     // await ready;
+    //     const currentCommands = options.guildId ? await client.application!.commands.fetch({
+    //         guildId: options.guildId,
+    //     }) : await client.application!.commands.fetch();
 
-        console.log(`Synchronizing commands...`);
+    //     console.log(currentCommands)
 
-        const newCommands = commands.filter((command) => !currentCommands.some((c) => c.name === command.name));
-        for (let newCommand of newCommands) {
-            if (options.guildId) await client.application!.commands.create(newCommand, options.guildId);
-            else await client.application!.commands.create(newCommand);
-        }
+    //     console.log(`Synchronizing commands...`);
 
-        console.log(`Created ${newCommands.length} commands!`);
+    //     const newCommands = commands.filter((command) => !currentCommands.some((c) => c.name === command.name));
+    //     for (let newCommand of newCommands) {
+    //         if (options.guildId) await client.application.commands.create(newCommand, options.guildId);
+    //         else await client.application.commands.create(newCommand).catch(console.error);
 
-        const deletedCommands = currentCommands.filter((command) => !commands.some((c) => c.name === command.name)).toJSON();
-        for (let deletedCommand of deletedCommands) {
-            await deletedCommand.delete();
-        }
+    //     }
 
-        console.log(`Deleted ${deletedCommands.length} commands!`);
+    //     console.log(`Created ${newCommands.length} commands!`);
 
-        const updatedCommands = commands.filter((command) => currentCommands.some((c) => c.name === command.name));
-        let updatedCommandCount = 0;
-        for (let updatedCommand of updatedCommands) {
-            const newCommand = updatedCommand;
-            const previousCommand = currentCommands.find((c) => c.name === updatedCommand.name);
-            let modified = false;
-            if (previousCommand!.description !== newCommand.description) modified = true;
-            if (!ApplicationCommand.optionsEqual(previousCommand!.options ?? [], newCommand.options ?? [])) modified = true;
-            if (modified) {
-                await previousCommand!.edit(newCommand as unknown as ApplicationCommandData);
-                updatedCommandCount++;
-            }
-        }
+    //     const deletedCommands = currentCommands.filter((command) => !commands.some((c) => c.name === command.name)).toJSON();
+    //     for (let deletedCommand of deletedCommands) {
+    //         await deletedCommand.delete();
+    //     }
 
-        console.log(`Updated ${updatedCommandCount} commands!`);
+    //     console.log(`Deleted ${deletedCommands.length} commands!`);
 
-        console.log(`Commands synchronized!`);
+    //     const updatedCommands = commands.filter((command) => currentCommands.some((c) => c.name === command.name));
+    //     let updatedCommandCount = 0;
+    //     for (let updatedCommand of updatedCommands) {
+    //         const newCommand = updatedCommand;
+    //         const previousCommand = currentCommands.find((c) => c.name === updatedCommand.name);
+    //         let modified = false;
+    //         // if (previousCommand!.description !== newCommand.description) modified = true;
+    //         if (!ApplicationCommand.optionsEqual(previousCommand!.options ?? [], newCommand.options ?? [])) modified = true;
+    //         if (modified) {
+    //             await previousCommand!.edit(newCommand as unknown as ApplicationCommandData);
+    //             updatedCommandCount++;
+    //         }
+    //     }
 
-    }
+    //     console.log(`Updated ${updatedCommandCount} commands!`);
+
+    //     console.log(`Commands synchronized!`);
+
+    // }
 }
 
 export default SlashCommand;
